f60cfd3efbb80bd272292a10a7df66de8b53a1cf
Bayu Rahmad Azhari <bayurahmadazhari@gmail.com>
2017-03-28 11:39:08 +0700

    second commit

--- doc/tugas/tugas3.py
+++ doc/tugas/tugas3.py
@@ -1,23 +1,23 @@
 print("Wlayah A : Harimau, Ayam, Gabah")
 print("Manusia harus memindahkan semuanya dengan benar ke Wilayah B")
 a = raw_input('Pindahkan siapa ? : ')
-if a == "Ayam" or "ayam":
+if a == "ayam":
         print("\nWilayah A : Harimau, Gabah")
         print("Wilayah B : Ayam")
         b = raw_input('Lanjutkan, Pindahkan siapa ? : ')
-        if b == "Gabah" or "gabah":
+        if b == "gabah":
                 print("\nWilayah A : Harimau")
 		print("Wilayah B : Ayam, Gabah")
                 c = raw_input('Lanjutkan, Pindahkan siapa ? : ')
-                if c == "Ayam" or "ayam":
+                if c == "ayam":
                         print("\nWilayah A : Harimau, Ayam ")
                         print("Wilayah B : Gabah")
                         d = raw_input('Lanjutkan, Pindahkan siapa ? : ')
-                        if d == "Harimau" or "harimau":
+                        if d == "harimau":
                                 print("\nWlayah A : Ayam ")
                                 print("Wilayah B : Gabah, Harimau")
                                 e = raw_input('Lanjutkan, Pindahkan siapa ? : ')
-                                if e == "Ayam" or "ayam":
+                                if e == "ayam":
                                         print("\nWilayah B : Gabah, Harimau, Ayam")
                                         print("Selamat, Anda berhasil")
                                 else:
